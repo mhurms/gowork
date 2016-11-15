77c78dcf49809b778b462634eb43d461cc7058c8
mhurms <stuart.k.caldwell@gmail.com>
2016-11-15 21:47:33 +1100

    udemy progress

--- hello/hello.go
+++ hello/hello.go
@@ -7,6 +7,5 @@ import (
 )
 
 func main() {
-	fmt.Printf(string.Reverse("\nolleH, world!\n"))
-
+	fmt.Println(string.Reverse("\nolleH, world!\n"))
 }
--- numeralSystems/binary/main.go
+++ numeralSystems/binary/main.go
@@ -1,7 +1,21 @@
-package main
+/*package main
 
 import "fmt"
 
 func main() {
 	fmt.Printf("%d - %b\n", 42, 42)
+}
+*/
+
+package main
+
+import "fmt"
+
+const p = "death & taxes"
+const q = 42
+
+func main() {
+
+	fmt.Println("p - ", p)
+	fmt.Println("q - ", q)
 }
